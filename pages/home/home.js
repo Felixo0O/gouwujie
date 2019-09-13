7b06abd706504e9aaf5d30d75adfd229fcb0c148
Felixo0O <Felix_Web@163.com>
2019-09-13 15:02:55 +0800

    获取接口数据方法封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,72 +1,84 @@
 // pages/home/home.js
-import request from '../../service/network.js'
+// import request from '../../service/network.js'
+import {
+  getMultiData
+} from '../../service/home.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    banners:[],
+    recommends:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-      // 1.请求轮播图数据及推荐数据
-      request({
-        url:'http://123.207.32.32:8000/api/v1/home/multidata'
-      }).then(res => {
-        console.log(res)
+  onLoad: function(options) {
+    // 1.请求轮播图数据及推荐数据
+    getMultiData().then(res => {
+      // 取出轮播图和推荐数据
+      const banners = res.data.data.banner.list
+      const recommends = res.data.data.recommend.list
+
+      // 将banners和recommends放到data中
+      this.setData({
+        banners,
+        recommends
       })
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- service/network.js
+++ service/network.js
@@ -1,7 +1,11 @@
+import {
+  baseUrl
+} from './config.js'
+
 export default function (options){
   return new Promise((resolve,reject) => {
     wx.request({
-      url: options.url,
+      url: baseUrl + options.url,
       method: options.method || 'get',
       data: options.data || {},
       success: resolve,
