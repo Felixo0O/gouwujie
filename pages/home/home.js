dbf29eabdffe011bc01415d56d1d169faed88ee6
Felixo0O <Felix_Web@163.com>
2019-09-15 17:56:02 +0800

    开始渲染商品

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,8 +1,11 @@
 // pages/home/home.js
 // import request from '../../service/network.js'
 import {
-  getMultiData
+  getMultiData,
+  getGoodsData
 } from '../../service/home.js'
+
+const types = ['pop', 'new', 'sell']
 
 Page({
 
@@ -12,12 +15,23 @@ Page({
   data: {
     banners:[],
     recommends:[],
-    titles: ['流行', '新款', '精选']
+    titles: ['流行', '新款', '精选'],
+    goods: {
+      pop: {page: 0, list: []},
+      new: {page: 0, list: []},
+      sell: {page: 0, list: []}
+    },
+    currentType:'pop'
   },
   // 监听tabcontrol事件
   handleTabClick(event) {
     // 取出index
     const index = event.detail.index
+
+    // 设置 currentType
+    this.setData({
+      currentType:types[index]
+    })
   },
 
   /**
@@ -25,10 +39,18 @@ Page({
    */
   onLoad: function(options) {
     // 1.请求轮播图数据及推荐数据
+    this._getMultidata()
+    // 2.请求商品数据
+    this._getGoodsData('pop')
+    this._getGoodsData('new')
+    this._getGoodsData('sell')
+
+  },
+  _getMultidata() {
     getMultiData().then(res => {
-      // 取出轮播图和推荐数据
-      const banners = res.data.data.banner.list
-      const recommends = res.data.data.recommend.list
+      // 取出轮播图和推荐的数据
+      const banners = res.data.data.banner.list;
+      const recommends = res.data.data.recommend.list;
 
       // 将banners和recommends放到data中
       this.setData({
@@ -36,7 +58,29 @@ Page({
         recommends
       })
     })
+  },
+  _getGoodsData(type) {
+    // 1.获取页码
+    const page = this.data.goods[type].page + 1;
 
+    // 2.发送网络请求
+    getGoodsData(type, page).then(res => {
+      // console.log(res)
+      // 2.1.取出数据
+      const list = res.data.data.list;
+
+      // 2.2.将数据设置到对应type的list中
+      const oldList = this.data.goods[type].list;
+      oldList.push(...list)
+
+      // 2.3.将数据设置到data中的goods中
+      const typeKey = `goods.${type}.list`;
+      const pageKey = `goods.${type}.page`;
+      this.setData({
+        [typeKey]: oldList,
+        [pageKey]: page
+      })
+    })
   },
 
   /**
--- service/home.js
+++ service/home.js
@@ -6,3 +6,13 @@ export function getMultiData (){
     url: '/api/v1/home/multidata'
   })
 }
+
+export function getGoodsData(type,page) {
+  return request({
+    url: '/api/v1/home/data',
+    data:{
+      type,
+      page
+    }
+  })
+}
